b9ea6a383d777e7750ba822cc27f6ebf50dc4c92
Jarseneaux1 <joshuamarseneaux@gmail.com>
2023-08-17 19:31:24 -0400

    Initial commit

---
+++ src/main/java/projects/exception/Dbexception.java
@@ -0,0 +1,26 @@
+package projects.exception;
+
+@SuppressWarnings("serial")
+public class Dbexception extends RuntimeException {
+
+	public Dbexception() {
+		
+	}
+
+	public Dbexception(String message) {
+		super(message);
+		
+	}
+
+	public Dbexception(Throwable cause) {
+		super(cause);
+	
+	}
+
+	public Dbexception(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+}
+
+	
---
+++ src/main/java/projects/projects.java
@@ -0,0 +1,18 @@
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.Dbconnection;
+
+public class projects {
+
+	
+		public static void main(String[] args) {
+			Connection conn = Dbconnection.getConnection();
+		}
+
+	
+
+	
+
+}
